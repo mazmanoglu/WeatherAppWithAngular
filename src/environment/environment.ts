f230fc4b45368eef4498f9146c48f41c5ec0edf2
fatih ozer <fatihozer246@hotmail.com>
2022-12-10 12:10:24 +0100

    firstPush

---
+++ src/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://api.api-ninjas.com/v1/weather?city=',
+  XRapidApiKeyHeaderName: 'X-Api-Key',
+  XRapidApiKeyHeaderValue: '87Dr9Y0YHr+z/dvWJoyH/A==f76b0nC7tyGdo4rX',
+};
